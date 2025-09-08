5165e2861fd5029f677e9dcac9c842bf1d4176b6
NET-VLADOR <lolipop120999@gmail.com>
2025-09-09 03:02:11 +0500

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	base: 'birthday-deposit',
+	base: '/birthday-deposit/',
 	plugins: [react(), tailwindcss()],
 	test: {
 		environment: 'jsdom',
